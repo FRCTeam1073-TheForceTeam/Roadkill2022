687bc04ad7c21e65ceb8f7cf21e2ccf828754eeb
Oli <24oliviao@gmail.com>
2022-12-14 20:07:13 -0500

    added bling and rawbutton

---
+++ RookieTraining/src/main/java/frc/robot/subsystems/Bling.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Bling extends SubsystemBase{
+    public AddressableLED m_Led;
+    public AddressableLEDBuffer m_LedBuffer;
+
+    public int ledR = 0;
+    public int ledG = 0;
+    public int ledB = 0;
+
+    public Bling(){
+        m_Led = new AddressableLED(0);
+        m_LedBuffer = new AddressableLEDBuffer(8);
+        m_Led.setLength(m_LedBuffer.getLength());
+        m_Led.setData(m_LedBuffer);
+        m_Led.start();
+    }
+
+    @Override
+    public void periodic(){
+        m_Led.setData(m_LedBuffer);
+    }
+
+    public void setRGB(int i, int r, int g, int b){
+        m_LedBuffer.setRGB(i, r, g, b);
+    }
+
+    public void setRGBAll(int r, int g, int b){
+        ledB = r;
+        ledG = g;
+        ledB = b;
+        for(int i = 0; i < m_LedBuffer.getLength(); i++)
+        {
+            m_LedBuffer.setRGB(i, r, g, b);
+        }
+    }
+}
